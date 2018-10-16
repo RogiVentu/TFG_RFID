6d772beb4a739d80abeec2c7301a3fdd5da271f1
u114129 <u114129@50craip63.aulesca.upf.edu>
2018-10-16 18:39:08 +0200

    Json relevant data saved

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import json
+import codecs
+from pprint import pprint
 
 def createScene(size):
 	scene = []
@@ -11,19 +13,47 @@ def createScene(size):
 	return scene
 
 def readJSON():
-	with open('dades/dataTest.json') as json_data:
-		d = json.load(json_data)
-    	print(d)
+	data = []
+	with open('dades/dataTest.json','r') as f:
+		for line in f:
+			data.append(json.loads(line))
+
+	return data
+
+def getRelevantVariables(data):
+#at the moment: epc,robot_pose,RSSI,ts,ANTENNA_PORT,device_id
+#return a list of dictionaries
+	relData = []
+	for d in data:
+		relData.append({'epc':d['epc'],
+			'robot_pose':d['robot_pose'],
+			'rssi':d['RSSI'],
+			'ts':d['ts'],
+			'antena_port':d['ANTENNA_PORT'],
+			'device_id':d['device_id']})
+
+	return relData
+
+def quatToDegrees():
+	# antenas L = left antenas R = right
 
 
-
-
-print "Creating scene..."
+print("Creating scene...")
 size = 10
 scene = createScene(size)
-print "Scene " +str(size)+ "x" +str(size)+ " created"
+print("Scene " +str(size)+ "x" +str(size)+ " created")
 
-readJSON()
+print("Reading data...")
+jsonData = readJSON()
+print("Done.")
+
+print("Getting important variables...")
+relevantData = getRelevantVariables(jsonData)
+print("Done.")
+
+
+
+
 
 
 #print "Write data... \nFormat: posX,posY,dir \nExample: 33,70,top"
