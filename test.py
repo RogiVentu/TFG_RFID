40d1e41bcaa47fe6d9b727577f4c8a8fe0ef0a36
Roger Ventura Castelló <roger.ventura01@estudiant.upf.edu>
2018-10-16 16:45:15 +0200

    ouhhh

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 
 def createScene(size):
 	scene = []
@@ -9,14 +10,22 @@ def createScene(size):
 			scene[i][j] = 0
 	return scene
 
+def readJSON():
+	with open('dades/dataTest.json') as json_data:
+		d = json.load(json_data)
+    	print(d)
+
+
+
 
 print "Creating scene..."
 size = 10
 scene = createScene(size)
 print "Scene " +str(size)+ "x" +str(size)+ " created"
 
-print "Write data... \nFormat: posX,posY,dir \nExample: 33,70,top"
+readJSON()
 
 
-x,y,dire = raw_input().split(",")
-print "Detection saved in position ("+str(x)+","+str(y)+") looking at " + dire + "."
+#print "Write data... \nFormat: posX,posY,dir \nExample: 33,70,top"
+#x,y,dire = raw_input().split(",")
+#print "Detection saved in position ("+str(x)+","+str(y)+") looking at " + dire + "."
